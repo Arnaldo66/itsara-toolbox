bde9b01fd65d34d7ae24373d14a5a083c36cc012
Arnaud <a.parseval@gmail.com>
2018-05-13 19:13:42 +0200

    insert into db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\UploadFormType;
+use App\Entity\Command;
 
 class HomeController extends Controller
 {
@@ -17,13 +18,57 @@ class HomeController extends Controller
         $form = $this->createForm(UploadFormType::class, null);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dump($_FILES);
             $file = fopen($_FILES['upload_form']['tmp_name']['export'], "r");
-            dump(fgetcsv($file));die();
+            $this->insertCSVInDB($file);
         }
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController', 'form' => $form->createView()
         ]);
+    }
+
+    private function insertCSVInDB($file)
+    {
+        $row = 1;
+        while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+            if($row > 1){
+                $this->createCommand($data);
+            }
+            $row++;
+        }
+        dump('ok');
+        die();
+    }
+
+    private function createCommand(array $data)
+    {
+        $command = new Command;
+        $command->setNumber($data[0]);
+        $command->setDateCommand($data[2]);
+        $command->setTypePayment($data[3]);
+        $command->setArticle($data[4]);
+        $command->setUnitPriceTTC($data[6]);
+        $command->setUnitPriceHT($data[7]);
+        $command->setShippingCoastHT($data[8]);
+        $command->setShippingCoastTTC($data[9]);
+        $command->setTVA($data[11]);
+        $command->setNbArticle($data[12]);
+        $command->setPriceTTC($data[13]);
+        $command->setPriceHT($data[14]);
+        $command->setName($data[19]);
+        $command->setFirstName($data[21]);
+        $command->setStreetComplement($data[22]);
+        $command->setStreet($data[23]);
+        $command->setPostalCode($data[24]);
+        $command->setCity($data[25]);
+        $command->setProvince($data[26]);
+        $command->setCountry($data[27]);
+        $command->setPhone($data[28]);
+        $command->setEmail($data[29]);
+        $command->setPrint($data[29]);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($command);
+        $em->flush();
     }
 }
--- src/Entity/Command.php
+++ src/Entity/Command.php
@@ -17,9 +17,119 @@ class Command
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $number;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dateCommand;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $typePayment;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $article;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $unitPriceTTC;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $unitPriceHT;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $shippingCoastHT;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $shippingCoastTTC;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $streetComplement;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $postalCode;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $province;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $priceTTC;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $priceHT;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $TVA;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nbArticle;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $print;
 
     public function getId()
     {
@@ -34,6 +144,270 @@ class Command
     public function setNumber(string $number): self
     {
         $this->number = $number;
+
+        return $this;
+    }
+
+    public function getdateCommand(): ?string
+    {
+        return $this->dateCommand;
+    }
+
+    public function setdateCommand(?string $dateCommand): self
+    {
+        $this->dateCommand = $dateCommand;
+
+        return $this;
+    }
+
+    public function getTypePayment(): ?string
+    {
+        return $this->typePayment;
+    }
+
+    public function setTypePayment(?string $typePayment): self
+    {
+        $this->typePayment = $typePayment;
+
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?string $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+
+    public function getUnitPriceTTC(): ?string
+    {
+        return $this->unitPriceTTC;
+    }
+
+    public function setUnitPriceTTC(?string $unitPriceTTC): self
+    {
+        $this->unitPriceTTC = $unitPriceTTC;
+
+        return $this;
+    }
+
+    public function getUnitPriceHT(): ?string
+    {
+        return $this->unitPriceHT;
+    }
+
+    public function setUnitPriceHT(?string $unitPriceHT): self
+    {
+        $this->unitPriceHT = $unitPriceHT;
+
+        return $this;
+    }
+
+    public function getShippingCoastHT(): ?string
+    {
+        return $this->shippingCoastHT;
+    }
+
+    public function setShippingCoastHT(?string $shippingCoastHT): self
+    {
+        $this->shippingCoastHT = $shippingCoastHT;
+
+        return $this;
+    }
+
+    public function getShippingCoastTTC(): ?string
+    {
+        return $this->shippingCoastTTC;
+    }
+
+    public function setShippingCoastTTC(?string $shippingCoastTTC): self
+    {
+        $this->shippingCoastTTC = $shippingCoastTTC;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(?string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getStreetComplement(): ?string
+    {
+        return $this->streetComplement;
+    }
+
+    public function setStreetComplement(?string $streetComplement): self
+    {
+        $this->streetComplement = $streetComplement;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(?string $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getProvince(): ?string
+    {
+        return $this->province;
+    }
+
+    public function setProvince(?string $province): self
+    {
+        $this->province = $province;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPriceTTC(): ?string
+    {
+        return $this->priceTTC;
+    }
+
+    public function setPriceTTC(?string $priceTTC): self
+    {
+        $this->priceTTC = $priceTTC;
+
+        return $this;
+    }
+
+    public function getPriceHT(): ?string
+    {
+        return $this->priceHT;
+    }
+
+    public function setPriceHT(?string $priceHT): self
+    {
+        $this->priceHT = $priceHT;
+
+        return $this;
+    }
+
+    public function getTVA(): ?string
+    {
+        return $this->TVA;
+    }
+
+    public function setTVA(?string $TVA): self
+    {
+        $this->TVA = $TVA;
+
+        return $this;
+    }
+
+    public function getNbArticle(): ?string
+    {
+        return $this->nbArticle;
+    }
+
+    public function setNbArticle(?string $nbArticle): self
+    {
+        $this->nbArticle = $nbArticle;
+
+        return $this;
+    }
+
+    public function getPrint(): ?bool
+    {
+        return $this->print;
+    }
+
+    public function setPrint(bool $print): self
+    {
+        $this->print = $print;
 
         return $this;
     }
