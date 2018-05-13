6b711d68efc158da75eb722224c57521bc932bd9
Arnaud <a.parseval@gmail.com>
2018-05-13 15:24:23 +0200

    form front upload

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,26 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\UploadFormType;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $form = $this->createForm(UploadFormType::class, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form;
+            dump($_FILES);
+            dump(file_get_contents($_FILES['upload_form']['tmp_name']['export']));die();
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'HomeController', 'form' => $form->createView()
         ]);
     }
 }
